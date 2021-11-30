db0d9bdf481ad234c6bfda5b7d0436bbfdabb11c
Gert-Ranno Lumine <gert-ranno.kask@khk.ee>
2021-11-30 16:19:43 +0200

    lisasin kustutamis funktsiooni

--- app.js
+++ app.js
@@ -3,6 +3,24 @@
 const form =document.querySelector('form')
 form.addEventListener('submit',addTask)
 
+// task list
+const taskList = document.querySelector('ul');
+taskList.addEventListener("click", delTask)
+
+// delTask
+function delTask(event) {
+
+    if(event.target.textContent === 'X') {
+
+        if(confirm('Do you really want to delete this task?')){
+
+            event.target.parentElement.remove()
+        }
+        console.log(event.target.parentElement)
+    }
+
+}
+
 // ADD TASK FUNCTION
 
 function addTask(event) {
@@ -18,6 +36,16 @@ function addTask(event) {
     // add text to element
     const text = document.createTextNode(task)
     li.appendChild(text)
+    // crete link
+    const link = document.createElement('a')
+    // add css style
+    link.className = 'secondary-content'
+    // add text
+    link.appendChild(document.createTextNode('X'))
+    // add href attribute
+    link.setAttribute('href' , '#')
+    // add link to li
+    li.appendChild(link)
     //ADD LI TO TASK LIST
     taskList.appendChild(li)
     //clear form input value
